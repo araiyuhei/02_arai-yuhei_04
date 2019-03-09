71a1dbea46642fdd7dde23a16d0adcfa65c16863
araiyuhei <cocoa5554@gmail.com>
2019-03-09 22:19:14 +0900

    no message

--- 課題2/php01/index.php
+++ 課題2/php01/index.php
@@ -1,50 +1,62 @@
+<?php
+$result = false;
+if (isset($_POST['choice'])) {
+    $janken = array(
+        'グー',
+        'チョキ',
+        'パー'
+    );
+    $player = htmlspecialchars($_POST['choice'], ENT_QUOTES, 'UTF-8');
+    $com = $janken[array_rand($janken)];
+
+    if ($player === 'グー' && $com === 'チョキ') {
+        $result = '勝ち';
+    } elseif ($player === 'グー' && $com === 'グー') {
+        $result = 'あいこ';
+    } elseif ($player === 'グー' && $com === 'パー') {
+        $result = '負け';
+    } elseif ($player === 'チョキ' && $com === 'グー') {
+        $result = '負け';
+    } elseif ($player === 'チョキ' && $com === 'パー') {
+        $result = '勝ち';
+    } elseif ($player === 'チョキ' && $com === 'チョキ') {
+        $result = 'あいこ';
+    } elseif ($player === 'パー' && $com === 'チョキ') {
+        $result = '負け';
+    } elseif ($player === 'パー' && $com === 'グー') {
+        $result = '勝ち';
+    } elseif($player === 'パー' && $com === 'パー') {
+        $result = 'あいこ';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="test.css">
+<title>じゃんけん</title>
 </head>
 <body>
-<h5>PHPの書き方</h5>
-<?php 
-    echo'<h3>このように書いていく</h3>';
-    echo 5+2 ;
-    echo '<br>';
-    echo'5+2';
-    echo '<br>';
-    $apple ="リンゴ";
-    echo $apple;
-    echo '<br>';
-    $num = 10+10;
-    echo $num;
-    echo '<br>';
+    <div class ="text-center">
+<p>あなたの手を選んでください。</p>
 
-    $a=1;
-    if ($a==2) {
-        echo "回答は１である";
-    }else{
-        echo "回答は１ではない";
-    }
-    echo '<br>';
+<form action="" method="post">
+    <button type="submit" name="choice" value="グー">グー</button>
+    <button type="submit" name="choice" value="チョキ">チョキ</button>
+    <button type="submit" name="choice" value="パー">パー</button>
+</form>
 
-    $b=2;
-    if($a="2"){
-        echo "回答は2である";
-    }else{
-        echo "回答は2ではない";
-    }
-    echo '<br>';
-    
-    echo mt_rand(1, 15);
+<?php if ($result) : ?>
+    <h1><p>結果</p></h1>
 
+    <p>
+    あなた：<?php echo $player; ?><br>
+    あいて：<?php echo $com; ?>
+    </p>
 
-
-
-
-
-
-?>    
+    <p><?php echo $result; ?>です。</p>
+<?php endif; ?>
+</div>
 </body>
 </html>
